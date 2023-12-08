94592baa9930ded6e7db59790b1ec4cfbea2554d
makaszy <m.a.kaszynski@protonmail.com>
2023-12-08 12:40:09 +0100

    Refactor toPostfix to accept and return space separated strings
    Change toPostfix input to strings separated by spaces to conserve multi-digit numbers. Change output to strings with operands and operators separated by spaces to allow multi-digit solve.

--- src/calculation-module/to-postfix-function/toPostfix.js
+++ src/calculation-module/to-postfix-function/toPostfix.js
@@ -2,28 +2,27 @@ import getPrec from './operator-precedence-function/getOperatorPrecedence'; // g
 
 function toPostfix(str) {
   const stack = []; // stack for operators
-  let result = ''; // final result string
+  const result = []; // final result string
   const isOperand = (char) => !Number.isNaN(Number(char)); // checks if char is a number
   const handleOpeningParenthesis = (char) => stack.push(char);
   const handleClosingParenthesis = () => {
     while (stack.length !== 0 && stack[stack.length - 1] !== '(') {
-      result += stack.pop();
+      result.push(stack.pop());
     }
     stack.pop(); // remove the opening parenthesis
   };
 
   const handleOperator = (char) => {
     while (stack.length !== 0 && getPrec(char) <= getPrec(stack[stack.length - 1])) {
-      result += stack.pop();
+      result.push(stack.pop());
       // pop stack to result until the top operator has less precedence or the stack is empty
     }
     stack.push(char);
     // push operator to stack
   };
-  for (let i = 0; i < str.length; i += 1) { // iterate through the string
-    const currentChar = str[i];
+  str.split(' ').forEach((currentChar) => {
     if (isOperand(currentChar)) {
-      result += currentChar;
+      result.push(currentChar);
     } else if (currentChar === '(') {
       handleOpeningParenthesis(currentChar);
     } else if (currentChar === ')') {
@@ -31,11 +30,11 @@ function toPostfix(str) {
     } else {
       handleOperator(currentChar);
     }
-  }
+  });
   while (stack.length !== 0) { // pop the remaining to the result
-    result += stack.pop();
+    result.push(stack.pop());
   }
-  return result;
+  return result.join(' ');
 }
 
 export default toPostfix;
