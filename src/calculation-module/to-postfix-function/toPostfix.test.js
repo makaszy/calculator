5c62afcbf8cccce68b783375ad224078eb9b1ee7
makaszy <m.a.kaszynski@protonmail.com>
2023-12-10 12:27:29 +0100

    Add additional test to toPostfix.test.js

--- src/calculation-module/to-postfix-function/toPostfix.test.js
+++ src/calculation-module/to-postfix-function/toPostfix.test.js
@@ -19,3 +19,8 @@ test('toPostFix: () && ^ && * && /', () => {
   expect(toPostfix('2000 - 501 + ( 30 - 5 ) * 60 / 3')).toBe('2000 501 - 30 5 - 60 * 3 / +');
   expect(toPostfix('2 ^ 3 - 501 + ( 30 - 5 ) * 60 / 3')).toBe('2 3 ^ 501 - 30 5 - 60 * 3 / +');
 });
+
+test('toPostFix: number input', () => {
+  expect(toPostfix('23')).toBe('23');
+  expect(toPostfix('2')).toBe('2');
+});
