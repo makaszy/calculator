f39ba1ce7dbe2a88d28bfe9bde182db6dd9e8e94
makaszy <m.a.kaszynski@protonmail.com>
2023-12-13 18:54:51 +0100

    Refactor operationInput

--- src/js/interface-module/interface.js
+++ src/js/interface-module/interface.js
@@ -1,9 +1,158 @@
 import PubSub from '../pub-sub-module/PubSub';
 import solveInfix from '../calculation-module/solveInfix';
 
-const operationInput = {
+const operation = {
+  valueArr: [],
+  pubSub: new PubSub(),
+
+  publishChange() {
+    this.pubSub.publish({
+      currentValue: this.valueArr.join(' '),
+      preppedValue: this.prepIncompleteValueArr().join(' '),
+    });
+  },
+
+  lastValueIsType(type) {
+    if (this.valueArr.length === 0) {
+      return false;
+    }
+    const lastValue = this.valueArr[this.valueArr.length - 1];
+    switch (type) {
+      case 'operand': {
+        return !Number.isNaN(Number(lastValue));
+      }
+      case 'operator': {
+        const validOperators = ['+', '-', '*', '/', '^'];
+        return validOperators.includes(lastValue);
+      }
+      case 'start-parenthesis': {
+        return lastValue === '(';
+      }
+      case 'end-parenthesis': {
+        return lastValue === ')';
+      }
+      default:
+        return 'invalid type';
+    }
+  },
+  addOperand(operand) {
+    if (this.lastValueIsType('end-parenthesis')) {
+      console.log('please add an operator');
+      return; // replace with a pop up
+    }
+    if (this.lastValueIsType('operand')) {
+      this.valueArr[this.valueArr.length - 1] += operand;
+    } else {
+      this.valueArr.push(operand);
+    }
+    this.publishChange();
+  },
+  addOperator(operator) {
+    if (this.lastValueIsType('end-parenthesis')) {
+      console.log('please add an operand first'); // replace with a pop up
+      return;
+    }
+    if (this.lastValueIsType('operator')) {
+      this.valueArr.pop();
+    }
+    this.valueArr.push(operator);
+    this.publishChange();
+  },
+  addDecimal() {
+    if (this.lastValueIsType('operator') || this.valueArr.length === 0) {
+      this.valueArr.push('0.');
+    } else if (this.valueArr[this.valueArr.length - 1].includes('.')) {
+      console.log('only 1 decimal allowed');
+      return; // replace with a pop up;
+    } else if (this.lastValueIsType('end-parenthesis')) {
+      console.log('add an operator first');
+      return;
+    } else {
+      this.valueArr[this.valueArr.length - 1] += '.';
+    }
+    this.publishChange();
+  },
+  addParenthesisStart() {
+    if (this.valueArr[this.valueArr.length - 1] === 0 || this.lastValueIsType('operator') || this.lastValueIsType('parenthesis-start')) {
+      this.valueArr.push('(');
+      this.publishChange();
+    } else {
+      console.log('parenthesis require an operator');
+    }
+  },
+  addParenthesisEnd() {
+    const parenthesisStart = this.valueArr.filter((section) => section === '(');
+    const parenthesisEnd = this.valueArr.filter((section) => section === ')');
+    if (parenthesisStart.length > parenthesisEnd.length) {
+      this.valueArr.push(')');
+    } else {
+      console.log('Start parenthesis missing');
+    }
+  },
+  addMissingParenthesisEnd(arr) {
+    const parenthesisStart = this.valueArr.filter((section) => section === '(');
+    const parenthesisEnd = this.valueArr.filter((section) => section === ')');
+    if (parenthesisStart.length !== parenthesisEnd.length) {
+      while (parenthesisStart.length > parenthesisEnd.length) {
+        arr.push(')');
+        parenthesisEnd.push(')');
+      }
+    }
+    return arr;
+  },
+  prepIncompleteValueArr() {
+    let arr = this.valueArr.slice();
+    if (this.lastValueIsType('operator')) {
+      arr.pop();
+    }
+    arr = this.addMissingParenthesisEnd(arr);
+    return arr;
+  },
+  dltValue() {
+    if (this.valueArr.length !== 0) {
+      if (!this.lastValueIsType('operand')) {
+        this.valueArr.pop();
+      } else {
+        let lastElement = this.valueArr[this.valueArr.length - 1];
+        lastElement = lastElement.substring(0, this.valueArr.length - 1);
+        if (this.valueArr[this.valueArr.length - 1].length === 0) {
+          this.valueArr.pop();
+        }
+      }
+      this.publishChange();
+    }
+  },
+};
+
+/* const operationInput = {
   input: document.querySelector('.display__input--operation'),
-  lastCharIsOperator() {
+ */
+  /* getLastCharsSubstr(num) {
+    const value = this.getValue();
+    return value.substring(value.length - num)
+  }
+
+  isType(type) {
+    if (this.input.value.length < 2) {
+      return false;
+    }
+    switch (type) {
+      case 'operator': {
+        const validOperators = ['+', '-', '*', '/', '^'];
+        const lastCharsTrimmed = this.getLastCharsSubstr(3).trim();
+        //const lastThreeCharTrimmed = value.substring(value.length - 3).trim();
+        return validOperators.includes(lastCharsTrimmed);
+      }
+      case 'start-parenthesis':
+        const lastTwoCharTrimmed = value.substring(value-length - 2).trim();
+
+        return value.charAt(va);
+      default:
+        return 'unknown parameter';
+    }
+  }, */
+
+/*   lastCharIsOperator() {
     if (this.input.value.length < 2) {
       return false;
     }
@@ -15,20 +164,31 @@ const operationInput = {
     }
     return this.input.value.charAt(this.input.value.length - 1) === ')';
   },
-  setValue(value) {
+  lastCharIsStartParenthesis() {
+    if (this.input.value.length < 2) {
+      return false;
+    }
+
+  }, */
+/*   setValue(value) {
     if (this.lastCharIsEndParenthesis()) {
       console.log('please add an operator'); // replace with a pop up
     } else {
       this.input.value += value;
     }
-  },
-  setOperator(value) {
-    if (this.lastCharIsOperator()) {
+  }, */
+/*   setOperator(value) {
+    if (this.lastCharIsOperator() && !this.lastCharIsEndParenthesis()) {
       this.input.value = this.input.value.substring(0, this.input.value.length - 3);
+      this.input.value += value;
+    } else if (this.lastCharIsEndParenthesis()) {
+      console.log('please set an operand');
+    } else {
+      this.input.value += value;
     }
-    this.input.value += value;
-  },
-  setDecimal(value) {
+    
+  }, */
+/*   setDecimal(value) {
     if (this.lastCharIsOperator()) {
       this.input.value += `0${value}`;
     } else {
@@ -40,8 +200,8 @@ const operationInput = {
         this.input.value += value;
       }
     }
-  },
-  del() {
+  }, */
+/*   del() {
     if (this.lastCharIsOperator()) {
       this.input.value = this.input.value.substring(0, this.input.value.length - 3);
     } else if(this.lastCharIsEndParenthesis()) {
@@ -49,8 +209,8 @@ const operationInput = {
     } else {
       this.input.value = this.input.value.substring(0, this.input.value.length - 1);
     }
-  },
-  setParenthesisStart(value) {
+  }, */
+/*   setParenthesisStart(value) {
     if (this.input.value.length === 0 || this.lastCharIsOperator()) {
       this.input.value += value;
     } else if (!this.lastCharIsOperator()) {
@@ -66,8 +226,8 @@ const operationInput = {
     } else {
       console.log('Start parenthesis missing');
     }
-  },
-  solveValue() {
+  }, */
+ /*  solveValue() {
     if (this.lastCharIsOperator()) {
       this.input.value = this.input.value.substring(0, this.input.value.length - 3);
     }
@@ -82,12 +242,12 @@ const operationInput = {
       return this.input.value;
     }
     return this.input.value;
-  },
+  }, */
 
-  getValue() {
+/*   getValue() {
     return this.input.value;
   },
-};
+}; */
 // operand keys
 const operandPubSub = new PubSub();
 operandPubSub.subscribe(operationInput.setValue.bind(operationInput));
@@ -160,4 +320,3 @@ const historyInput = {
 };
 
 solvePubSub.subscribe(historyInput.addOperation.bind(historyInput));
-
